0dad2eb9dc053cbd6deb49043934decb55129e22
Samson Rozansky <samsonrozansky@gmail.com>
2025-12-25 15:50:59 -0500

    Fix Spotify player lag when switching themes
    - Remove dynamic theme parameter from Spotify iframe URL
    - Keep Spotify in dark theme consistently to prevent reloading
    - Add lazy loading attribute for better performance

--- src/App.jsx
+++ src/App.jsx
@@ -717,12 +717,13 @@ function PersonalPage({ isDark }) {
             <div className="spotify-embed">
               <iframe
                 title="Spotify Playlist"
-                src={`https://open.spotify.com/embed/playlist/7ttYj7jv5TS2UEuVSaTzd1?utm_source=generator${isDark ? '&theme=0' : ''}`}
+                src="https://open.spotify.com/embed/playlist/7ttYj7jv5TS2UEuVSaTzd1?utm_source=generator&theme=0"
                 width="100%"
                 height="352"
                 frameBorder="0"
                 allowFullScreen=""
                 allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                loading="lazy"
               ></iframe>
             </div>
           </div>
